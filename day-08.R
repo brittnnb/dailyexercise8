56edda0d9cc2a48ec7163bd629e7938bcd0136a3
Brittany <brittnnb@colostate.edu>
2025-02-28 08:48:45 -0700

    Initial commit

---
+++ day-08.R
@@ -0,0 +1,16 @@
+# Brittany Bobb
+# 02/24/2025
+# script is for exercise 8
+library(tidyverse)
+library(ggplot2)
+library(ggthemes)
+
+url<-"https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
+covid<-read_csv(url)
+
+
+df= data.frame(region = state.region,
+               state = state.name,
+               state_abbreviation = state.abb)|>
+inner_join(df,covid,by = "state")|>
+summarise("state","date")
