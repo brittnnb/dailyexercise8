56846369580baafe0451842449f060081f61ac5e
Brittany <brittnnb@colostate.edu>
2025-02-28 09:51:53 -0700

    code

--- day-08.R
+++ day-08.R
@@ -8,10 +8,27 @@ library(ggthemes)
 url<-"https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
 covid<-read_csv(url)
 
-
 df= data.frame(region = state.region,
                state = state.name,
-               state_abbreviation = state.abb)|>
-inner_join(df,covid,by = "state")|>
-summarise("state","date")
+               state_abbreviation = state.abb)
+    inner_join(df,covid,by = "state")|>
+    group_by(region,date)|>
+    summarise(cases= sum(cases),deaths= sum(deaths))|>
+    pivot_longer(cols = c(cases, deaths),
+             names_to = "type",
+             values_to = "count") |>
+    ggplot(aes(x = date, y = count)) +
+    geom_line() +
+    facet_grid(type~region, scales = "free_y")+
+    theme_bw()
+
+    
+    
+    
+  
+
+  
+
+
+
 
